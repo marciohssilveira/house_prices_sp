a43c6028ea7025423783b3331cd58b3a7b080e0c
Marcio Silveira <mhsilveira.br@gmail.com>
2020-05-04 09:54:32 -0300

    added knnregressor model

--- data_prep.py
+++ data_prep.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 import math
 import time
 start_time = time.time()
@@ -12,13 +13,16 @@ https://www.kaggle.com/thiagodsd/sao-paulo-metro
 the main goal of this work is to find a apartment in SP that suits the needs of people
 """
 # Importing the data:
-estate = pd.read_csv('sao-paulo-properties-april-2019.csv')
-estate.columns = estate.columns.str.lower().str.replace(' ', '_')
+# House prices data
+house_prices = pd.read_csv('sao-paulo-properties-april-2019.csv')
+house_prices.columns = house_prices.columns.str.lower().str.replace(' ', '_')
 
+# Sao Paulo Metro Stations data
 metro = pd.read_csv('metrosp_stations.csv', index_col='station', sep=';')
 
 # Extracting some useful information for calculating the distances
-properties_coords = list(zip(estate['longitude'], estate['latitude']))
+# between the houses and the metro stations
+properties_coords = list(zip(house_prices['longitude'], house_prices['latitude']))
 stations_coords = list(zip(metro['lon'], metro['lat']))
 station_names = list(metro.index)
 
@@ -66,10 +70,47 @@ property_distances = get_min_distance(properties_coords, stations_coords, statio
 
 
 # Unpacking the result of the function and inserting them as new columns into the estate dataframe.
-estate['nearest_station'] = [names for names, distance in property_distances]
-estate['nearest_station_distance'] = [distance for names, distance in property_distances]
+house_prices['nearest_station'] = [names for names, distance in property_distances]
+house_prices['nearest_station_distance'] = [distance for names, distance in property_distances]
 
-# Saving the new data into the csv file
-estate.to_csv('real_estate_sp.csv', index=False)
+# From now on we'll be working only with properties for rent
+# Filtering the data according to the negotiation type
+rent = house_prices[house_prices['negotiation_type'] == 'rent']
+# sale = house_prices[house_prices['negotiation_type'] == 'sale']
+
+# Filtering the data according to the distance to the nearest subway station
+# rent = rent[rent['nearest_station_distance'] <= 1]
+
+# Numerical and Categorical features
+# numerical = rent.dtypes[rent.dtypes != "object"].index
+# categorical = rent.dtypes[rent.dtypes == "object"].index
+
+
+def remove_outliers(dataframe):
+    """
+    Interquartile range (IQR) is a measure of statistical dispersion,
+    being equal to the difference between 75th and 25th percentiles.
+
+    This function removes outliers from the given dataframe using
+    """
+    Q1 = dataframe.quantile(0.25)
+    Q3 = dataframe.quantile(0.75)
+    IQR = Q3 - Q1
+    updated = dataframe[~((dataframe < (Q1 - 1.5 * IQR)) | (dataframe > (Q3 + 1.5 * IQR))).any(axis=1)]
+    return updated
+
+
+rent_updated = remove_outliers(rent)
+
+# Comparing the data before and after removing outliers with IQR technique
+fig, axs = plt.subplots(1, 2)
+axs[0].boxplot(rent['price'])
+axs[0].set_title('With Outliers')
+axs[1].boxplot(rent_updated['price'])
+axs[1].set_title('Without outliers')
+plt.show()
+
+# Storing the rent data in a new csv
+rent_updated.to_csv('rent_data.csv', index=False)
 
 print(f"--- the script ran in {time.time() - start_time} seconds --- ")
